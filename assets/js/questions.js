407d504436f991f8efe78f0501e8470946a49420
Josh484 <josh.m.mendoza1@gmail.com>
2023-12-01 20:02:52 +0000

    added three dummy functions for each round of questions. Added a move function so that when the user answers a new round can begin with different questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -3,17 +3,20 @@ var choices = document.querySelector("#choices");
 var questionTitle =  document.querySelector("#question-title");
 var button = document.querySelector("#start");
 var startScreen = document.querySelector("#start-screen");
-
-
+var counter = 0;
+var arr = [roundTwo, roundThree];
+ 
 button.addEventListener("click", function(){
     startScreen.remove();
     roundOne();
 });
 
+
 function roundOne(){
     question.className = "";
     var quiz = [answer1= {detail: "true", right: true}, 
     answer2= {detail: "false", right: false}];
+    
     for (var i = 0; i < quiz.length; i++) {
         var newQuestion = document.createElement("p");
         newQuestion.textContent = quiz[i].detail;
@@ -27,16 +30,40 @@ function roundOne(){
 }
 
 function roundTwo(){
+    var quiz = [answer1= {detail: "true", right: true}, 
+    answer2= {detail: "false", right: false}];
+    
+    for (var i = 0; i < quiz.length; i++) {
+        var newQuestion = document.createElement("p");
+        newQuestion.textContent = quiz[i].detail;
+        newQuestion.setAttribute("data-question", i);
+        if(newQuestion.getAttribute("data-question") == 0){
+            newQuestion.setAttribute("data-question", "answer");
+        }
+        choices.appendChild(newQuestion);
+    }
 
+}
+
+function roundThree() {
     var quiz = [answer1= {detail: "true", right: true}, 
     answer2= {detail: "false", right: false}];
     for (var i = 0; i < quiz.length; i++) {
         var newQuestion = document.createElement("p");
         newQuestion.textContent = quiz[i].detail;
         newQuestion.setAttribute("data-question", i);
+        if(newQuestion.getAttribute("data-question") == 0){
+            newQuestion.setAttribute("data-question", "answer");
+        }
         choices.appendChild(newQuestion);
     }
+}
 
+function moveArr() {
+    if ( counter < arr.length) {
+        arr[counter]();
+        counter++;
+    }
 }
 
 choices.addEventListener("click", function(event){
@@ -44,7 +71,9 @@ choices.addEventListener("click", function(event){
     if (element.matches("p") === true){
         var chosen = element.getAttribute("data-question");
         if(chosen == "answer") {
-            roundTwo();
+            moveArr();
         }
     }
 });
+
+
