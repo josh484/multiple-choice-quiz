4c22ebcf3e5d9950e34bbe8632937782acd221f3
Josh484 <josh.m.mendoza1@gmail.com>
2023-12-01 19:04:06 +0000

    function roundone created. When user presses button creates two p elements with true or false

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,14 +1,23 @@
-var question =  document.querySelector("#question");
+var question =  document.querySelector("#questions");
 var choices = document.querySelector("#choices");
 var questionTitle =  document.querySelector("#question-title");
 var button = document.querySelector("#start");
-const startScreen = document.querySelector("#start-screen");
+var startScreen = document.querySelector("#start-screen");
 
 
 button.addEventListener("click", function(){
     startScreen.remove();
+    roundOne();
 });
 
 function roundOne(){
-    
+    question.className = "";
+    var quiz = [answer1= {detail: "true", right: true}, 
+    answer2= {detail: "false", right: false}];
+    for (var i = 0; i < quiz.length; i++) {
+        var newQuestion = document.createElement("p");
+        newQuestion.textContent = quiz[i].detail;
+        newQuestion.setAttribute("data-question", i);
+        choices.appendChild(newQuestion);
+    }
 }
