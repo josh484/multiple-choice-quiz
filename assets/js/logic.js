1607e90b9d657fe54885d59190b83ed714a61aaa
Josh484 <josh.m.mendoza1@gmail.com>
2023-12-01 20:31:43 +0000

    changed all round functions to use just one function but takes in quiz element so that it goes through each quiz

---
+++ assets/js/logic.js
@@ -0,0 +1,54 @@
+var question =  document.querySelector("#questions");
+var choices = document.querySelector("#choices");
+var questionTitle =  document.querySelector("#question-title");
+var button = document.querySelector("#start");
+var startScreen = document.querySelector("#start-screen");
+var counter = 0;
+
+var newQuiz = [question2, question3];
+ 
+button.addEventListener("click", function(){
+    startScreen.remove();
+    question.className = "";
+    quizzes(question1);
+});
+
+function moveArr() {
+    if ( counter < newQuiz.length) {
+        quizzes(newQuiz[counter]);
+        counter++;
+    } 
+}
+choices.addEventListener("click", function(event){
+    var element = event.target
+    if (element.matches("p") === true){
+        var chosen = element.getAttribute("data-question");
+        if(chosen == "true") {
+            console.log("you got it");
+            removeStuff();
+            moveArr();
+        }
+    }
+});
+
+function removeStuff(){
+    while(choices.firstChild){
+        choices.removeChild(choices.lastChild);
+    }
+}
+
+function quizzes(quiz){
+    quiz = [answer1= {detail: "true", right: true}, 
+    answer2= {detail: "false", right: false}];
+    
+    for (var i = 0; i < quiz.length; i++) {
+        var newQuestion = document.createElement("p");
+        newQuestion.textContent = quiz[i].detail;
+        newQuestion.setAttribute("data-question", quiz[i].right);
+        if(newQuestion.getAttribute("data-question") == true){
+            newQuestion.setAttribute("data-question", "answer");
+        }
+        choices.appendChild(newQuestion);
+    }
+
+}
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,79 +1,12 @@
-var question =  document.querySelector("#questions");
-var choices = document.querySelector("#choices");
-var questionTitle =  document.querySelector("#question-title");
-var button = document.querySelector("#start");
-var startScreen = document.querySelector("#start-screen");
-var counter = 0;
-var arr = [roundTwo, roundThree];
- 
-button.addEventListener("click", function(){
-    startScreen.remove();
-    roundOne();
-});
+var question1 = [[answer1= {detail: "true", right: true}, 
+answer2= {detail: "false", right: false}]];
+var question2 = [[answer1= {detail: "whatsup", right: true}, 
+answer2= {detail: "false", right: false}]];
+var question3 = [[answer1= {detail: "true", right: true}, 
+answer2= {detail: "false", right: false}]];
 
 
-function roundOne(){
-    question.className = "";
-    var quiz = [answer1= {detail: "true", right: true}, 
-    answer2= {detail: "false", right: false}];
-    
-    for (var i = 0; i < quiz.length; i++) {
-        var newQuestion = document.createElement("p");
-        newQuestion.textContent = quiz[i].detail;
-        newQuestion.setAttribute("data-question", i);
-        if(newQuestion.getAttribute("data-question") == 0){
-            newQuestion.setAttribute("data-question", "answer");
-        }
-        choices.appendChild(newQuestion);
-    }
 
-}
 
-function roundTwo(){
-    var quiz = [answer1= {detail: "true", right: true}, 
-    answer2= {detail: "false", right: false}];
-    
-    for (var i = 0; i < quiz.length; i++) {
-        var newQuestion = document.createElement("p");
-        newQuestion.textContent = quiz[i].detail;
-        newQuestion.setAttribute("data-question", i);
-        if(newQuestion.getAttribute("data-question") == 0){
-            newQuestion.setAttribute("data-question", "answer");
-        }
-        choices.appendChild(newQuestion);
-    }
-
-}
-
-function roundThree() {
-    var quiz = [answer1= {detail: "true", right: true}, 
-    answer2= {detail: "false", right: false}];
-    for (var i = 0; i < quiz.length; i++) {
-        var newQuestion = document.createElement("p");
-        newQuestion.textContent = quiz[i].detail;
-        newQuestion.setAttribute("data-question", i);
-        if(newQuestion.getAttribute("data-question") == 0){
-            newQuestion.setAttribute("data-question", "answer");
-        }
-        choices.appendChild(newQuestion);
-    }
-}
-
-function moveArr() {
-    if ( counter < arr.length) {
-        arr[counter]();
-        counter++;
-    }
-}
-
-choices.addEventListener("click", function(event){
-    var element = event.target
-    if (element.matches("p") === true){
-        var chosen = element.getAttribute("data-question");
-        if(chosen == "answer") {
-            moveArr();
-        }
-    }
-});
 
 
