0b93b552f15c4a8bd9e6e0ae02c41a573bbd3e2a
Josh484 <josh.m.mendoza1@gmail.com>
2023-12-01 20:44:12 +0000

    changed all round functions to use just one function but takes in quiz element so that it goes through each quiz

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,12 +4,14 @@ var questionTitle =  document.querySelector("#question-title");
 var button = document.querySelector("#start");
 var startScreen = document.querySelector("#start-screen");
 var counter = 0;
+var currentGameScore = 0;
 
 var newQuiz = [question2, question3];
  
 button.addEventListener("click", function(){
     startScreen.remove();
     question.className = "";
+    localStorage.setItem("score", currentGameScore++);
     quizzes(question1);
 });
 
@@ -25,6 +27,7 @@ choices.addEventListener("click", function(event){
         var chosen = element.getAttribute("data-question");
         if(chosen == "true") {
             console.log("you got it");
+            localStorage.setItem("score", currentGameScore++);
             removeStuff();
             moveArr();
         }
@@ -38,16 +41,11 @@ function removeStuff(){
 }
 
 function quizzes(quiz){
-    quiz = [answer1= {detail: "true", right: true}, 
-    answer2= {detail: "false", right: false}];
-    
+
     for (var i = 0; i < quiz.length; i++) {
         var newQuestion = document.createElement("p");
         newQuestion.textContent = quiz[i].detail;
         newQuestion.setAttribute("data-question", quiz[i].right);
-        if(newQuestion.getAttribute("data-question") == true){
-            newQuestion.setAttribute("data-question", "answer");
-        }
         choices.appendChild(newQuestion);
     }
 
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,9 +1,9 @@
-var question1 = [[answer1= {detail: "true", right: true}, 
-answer2= {detail: "false", right: false}]];
-var question2 = [[answer1= {detail: "whatsup", right: true}, 
-answer2= {detail: "false", right: false}]];
-var question3 = [[answer1= {detail: "true", right: true}, 
-answer2= {detail: "false", right: false}]];
+var question1 = [answer1= {detail: "true", right: true}, 
+answer2= {detail: "false", right: false}];
+var question2 = [answer1= {detail: "whatsup", right: true}, 
+answer2= {detail: "false", right: false}];
+var question3 = [answer1= {detail: "true", right: true}, 
+answer2= {detail: "false", right: false}];
 
 
 
