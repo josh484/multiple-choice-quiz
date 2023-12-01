8a0a2669dd74d2229f19192dac3dfff34a6bdbcc
Josh484 <josh.m.mendoza1@gmail.com>
2023-12-01 18:33:07 +0000

    added starting variables. created listener for button to remove quiz text

---
+++ assets/js/questions.js
@@ -0,0 +1,14 @@
+var question =  document.querySelector("#question");
+var choices = document.querySelector("#choices");
+var questionTitle =  document.querySelector("#question-title");
+var button = document.querySelector("#start");
+const startScreen = document.querySelector("#start-screen");
+
+
+button.addEventListener("click", function(){
+    startScreen.remove();
+});
+
+function roundOne(){
+    
+}
